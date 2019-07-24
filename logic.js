59c8cba66134217453d908aa7118c6160a532a79
Ryan Mobley <ryan.mobley94@gmail.com>
2019-07-23 20:09:13 -0500

    hello world

---
+++ logic.js
@@ -0,0 +1,9 @@
+$(window).on("load",()=>{
+    var ny_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=FgQwXVCpv81qt1OY2S84n9YTqjLVHlvD"
+    $.ajax({
+
+    }).then((response)=>{
+
+    });
+
+});
