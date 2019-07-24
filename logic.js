734a394d411ea5a1619f4f6cfe0bfa296873cf52
Ryan Mobley <ryan.mobley94@gmail.com>
2019-07-23 20:49:24 -0500

    working on api key

--- logic.js
+++ logic.js
@@ -1,6 +1,11 @@
 $(window).on("load",()=>{
-    var condition = "computers";
-    var ny_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+condition+"&api-key=FgQwXVCpv81qt1OY2S84n9YTqjLVHlvD";
+    var limit=5;
+    var year_start ="2015";
+    var year_end = "2019";
+    var term = "computers";
+    var ny_url_conditions="https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+term+"&offset="+limit+"&api-key=FgQwXVCpv81qt1OY2S84n9YTqjLVHlvD";
+
+    var ny_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+term+"&api-key=FgQwXVCpv81qt1OY2S84n9YTqjLVHlvD";
     $.ajax({
         url: ny_url,
         method: "GET"
